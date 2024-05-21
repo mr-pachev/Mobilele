f69ddf392136d1344e7c17ab419038b748f74b49
Ivan Pachev <mr.pachev@abv.bg>
2024-05-21 18:39:14 +0300

    Update LoginController

--- mobilele/src/main/java/bg/mobilele/web/LoginController.java
+++ mobilele/src/main/java/bg/mobilele/web/LoginController.java
@@ -1,14 +1,11 @@
 package bg.mobilele.web;
 
 import bg.mobilele.data.DTO.UserLoginDTO;
-import bg.mobilele.data.DTO.UserRegistrationDTO;
-import bg.mobilele.data.entity.User;
 import bg.mobilele.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -26,8 +23,7 @@ public class LoginController {
 
     @PostMapping("users/login")
     public String login(UserLoginDTO userLoginDTO){
-        userLoginDTO.getUsername();
 
-        return "redirect: index";
+        return "redirect:/";
     }
 }

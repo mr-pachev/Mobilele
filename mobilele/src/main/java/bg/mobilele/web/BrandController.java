48c7d8a22c0ee531049033ba586b3a97f80ffed8
Ivan Pachev <mr.pachev@abv.bg>
2024-09-13 17:39:33 +0300

    Added view in brands template.

--- mobilele/src/main/java/bg/mobilele/web/BrandController.java
+++ mobilele/src/main/java/bg/mobilele/web/BrandController.java
@@ -1,17 +1,30 @@
 package bg.mobilele.web;
 
+import bg.mobilele.service.BrandService;
+import bg.mobilele.service.ModelService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
-
 public class BrandController {
 
+    private final BrandService brandService;
+    private final ModelService modelService;
+
+    public BrandController(BrandService brandService, ModelService modelService) {
+        this.brandService = brandService;
+        this.modelService = modelService;
+    }
+
     @GetMapping("brands/all")
-    public ModelAndView brand() {
-        return new ModelAndView("brands");
+    public String viewBrands(Model model) {
+        model.addAttribute("brands", brandService.allBrands());
+        model.addAttribute("models", modelService.allModel());
+
+        return "brands";
     }
 }
